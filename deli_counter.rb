47e0f7d3a3ff81d563e33071d5c077af54b328f5
Lrd134 <larryc3200@gmail.com>
2021-03-04 16:38:54 -0500

    commit tree 38f14ba3905e01559c06338c0d7d0089d06f419e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def line(deli)
+    tmp_line = []
+    counter = 0
+    if deli.empty?
+      return puts "The line is currently empty."
+    end
+    deli.each do | in_line |
+      counter += 1
+      tmp_line << "#{counter}. #{in_line}"
+    end
+    tmp_line.unshift("The line is currently:")
+    puts tmp_line.join(" ")
+  end
+def take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    return puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli[0]}."
+    deli.shift
+  end
+end 
